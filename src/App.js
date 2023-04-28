7c48ca6955543b98c2f63349adf881b385507021
Lee Vue <vuelee123@gmail.com>
2023-04-28 18:13:27 -0400

    tic tac toe

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Index from './index.js';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        
+      </header>
+      <Index />
+    </div>
+  );
+}
+
+export default App;
